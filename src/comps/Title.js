eee535fdd042dd9332f4f339f72396dc40d181b0
irenealcaine <101673351+irenealcaine@users.noreply.github.com>
2023-07-18 09:52:26 +0200

    Initial commit

---
+++ src/comps/Title.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>Galería</h1>
+      <h2>Galería de fotos</h2>
+      <p>Sube tus fotos</p>
+    </div>
+  );
+};
+
+export default Title;
