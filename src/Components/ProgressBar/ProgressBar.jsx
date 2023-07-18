6c68bf73bad106ed99a9b8646d98c94ed86e16de
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-07-18 11:57:34 +0000

    Update ProgressBar.css and ProgressBar.jsx

--- src/Components/ProgressBar/ProgressBar.jsx
+++ src/Components/ProgressBar/ProgressBar.jsx
@@ -1,10 +1,18 @@
+import { useEffect } from "react";
 import useStorage from "../../Hooks/useStorage";
+import "./ProgressBar.css";
 
 const ProgressBar = ({ file, setFile }) => {
   const { url, progress } = useStorage(file);
   console.log(url, progress);
 
-  return <div className="progress-bar">progress</div>;
+  useEffect(() => {
+    if (url) {
+      setFile(null);
+    }
+  }, [url, setFile]);
+
+  return <div className="progress-bar" style={{ width: progress + "%" }}></div>;
 };
 
 export default ProgressBar;
