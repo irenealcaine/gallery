0c52c9186edb461dfeeb5865f89c8235c6c38c19
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-07-19 10:29:33 +0000

    Update 4 files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "./Components/Form/Form.jsx";
 import ImageGrid from "./Components/ImageGrid/ImageGrid.jsx";
+import Modal from "./Components/Modal/Modal.jsx";
 import Title from "./Components/Title/Title.jsx";
 
 function App() {
+  const [selectedImg, setSelectedImg] = useState(null);
+
   return (
     <div className="App">
       <Title />
       <Form />
-      <ImageGrid />
+      <ImageGrid setSelectedImg={setSelectedImg} />
+      {selectedImg && (
+        <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />
+      )}
     </div>
   );
 }
--- src/Components/ImageGrid/ImageGrid.jsx
+++ src/Components/ImageGrid/ImageGrid.jsx
@@ -1,7 +1,7 @@
 import useFirestore from "../../Hooks/useFirestore";
 import "./ImageGrid.css";
 
-const ImageGrid = () => {
+const ImageGrid = ({ setSelectedImg }) => {
   const { docs } = useFirestore("images");
   console.log(docs);
 
@@ -9,7 +9,11 @@ const ImageGrid = () => {
     <div className="image-grid">
       {docs &&
         docs.map((doc) => (
-          <div className="image-wrap" key={doc.id}>
+          <div
+            className="image-wrap"
+            key={doc.id}
+            onClick={() => setSelectedImg(doc.url)}
+          >
             <img src={doc.url} alt="uploadeed pic" />
           </div>
         ))}
